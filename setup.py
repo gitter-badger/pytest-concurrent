0b14fada2998c64f6cfcda01b6b93c68fc32a1f5
Reverb C <reverbc@users.noreply.github.com>
2017-08-10 23:37:23 -0700

    Update setup.py
    bump version to reflect doc change

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(fname):
 
 setup(
     name='pytest-concurrent',
-    version='0.1.0',
+    version='0.1.1',
     author='James Wang, Reverb Chu',
     author_email='jamesw96@uw.edu, reverbc@me.com',
     maintainer='James Wang, Reverb Chu',
